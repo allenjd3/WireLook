b3140d17dc5e39c2be6e1f96463ac56185374141
James Allen <jacques2186@yahoo.com>
2024-12-18 22:21:57 -0500

    Fix references

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
 <div>
     @livewireStyles
         @foreach($keys as $key)
             <div>
-                <a href="{{ route('wirelook', ['componentName' => $key]) }}">{{ $key }}</a>
+                <a href="{{ route('wirelook', ['component' => $key]) }}">{{ $key }}</a>
             </div>
         @endforeach
         @if (isset($componentName))
@@ -10,3 +18,5 @@
         @endif
     @livewireScripts
 </div>
+</body>
+</html>
