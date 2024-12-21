1e3089741ffc7fc1304adb2d36bc0699e8b2a291
James Allen <jacques2186@yahoo.com>
2024-12-21 07:13:29 -0500

    Add config to allow including stylesheets

--- config/wirelook.php
+++ config/wirelook.php
@@ -4,4 +4,5 @@
 return [
     'preview_path' => env('WIRELOOK_PREVIEW_PATH', app_path('Previews').'/'),
     'preview_namespace' => env('WIRELOOK_PREVIEW_NAMESPACE', 'App\\Previews\\'),
+    'stylesheet_paths' => ['resources/css/app.css'],
 ];
